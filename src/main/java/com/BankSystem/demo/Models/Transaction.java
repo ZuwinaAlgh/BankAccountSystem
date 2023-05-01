80cbcb575f7a617644c04c87f1fff2667438589b
ZuwinaAlgh <z.alghafri@hotmail.com>
2023-05-01 11:42:55 +0400

    create classes with their attributes

--- src/main/java/com/BankSystem/demo/Models/Transaction.java
+++ src/main/java/com/BankSystem/demo/Models/Transaction.java
@@ -8,7 +8,7 @@ import javax.xml.crypto.Data;
 @Entity
 public class Transaction {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    // to genarate primary key
     Integer id;
     Integer Amount;
     Data date;
